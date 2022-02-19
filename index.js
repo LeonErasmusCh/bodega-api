4cc30816bb8be12ead8d20fcfbf2f573a9cd68b7
Leon Erasmus <leondevcode@gmail.com>
2022-02-19 16:56:26 -0300

    heroku

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-var cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 
+const cors = require('cors')
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
