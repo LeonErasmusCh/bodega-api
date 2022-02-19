6bf62f7061ae54be20e36fd507fc164e55e849a8
Leon Erasmus <leondevcode@gmail.com>
2022-02-19 16:28:00 -0300

    Heroku settings

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3000
+
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(bodyParser.json())
