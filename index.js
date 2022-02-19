8b4d2215854c88fd94d3fef0f17914010476596e
Leon Erasmus <leondevcode@gmail.com>
2022-02-19 18:28:32 -0300

    Heroku

--- index.js
+++ index.js
@@ -6,8 +6,26 @@ const cors = require('cors')
 
 
 const app = express()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+
 app.use(cors());
 const port = process.env.PORT || 3000;
+
 
 
 app.use(bodyParser.json())
